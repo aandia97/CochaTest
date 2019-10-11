72c46d1592c2780a2006f51db09a088857e664a5
Alberto Andia <arieleo23@gmail.com>
2019-10-11 12:20:55 -0400

    Asignar funcionalidad a los botones y qe salga mensaje en la parte superior de la pregunta

--- app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
+++ app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
@@ -2,8 +2,11 @@ package io.github.n0g4y0.cochatest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botonVerdad = findViewById<Button>(R.id.verdad_button) as Button
+        botonVerdad.setOnClickListener {
+            val toast = Toast . makeText (this@MainActivity, "¡Correcto!", Toast.LENGTH_SHORT)
+            toast.show()
+            toast.setGravity(Gravity.TOP,0,500)
+
+        }
+        val botonFalso = findViewById<Button>(R.id.falso_button) as Button
+        botonFalso.setOnClickListener {
+            val toast1 = Toast.makeText(this@MainActivity,"¡Correcto!",Toast.LENGTH_SHORT)
+            toast1.show()
+            toast1.setGravity(Gravity.TOP,0,500)
+        }
     }
+}
+
+
+
+private operator fun View.invoke(layout: View?) {
+
 }
